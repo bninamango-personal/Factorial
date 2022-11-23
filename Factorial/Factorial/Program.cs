97e91c2c88f749292847c77b61d581c853edb169
bninamango.personal <bninamango.personal@gmail.com>
2022-11-22 21:25:42 -0500

    ...

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace bninamango
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Factorial : ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n == 0) return 1;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
